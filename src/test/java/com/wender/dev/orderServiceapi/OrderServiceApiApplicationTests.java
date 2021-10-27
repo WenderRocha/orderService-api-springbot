58ae46d4c98f06c370bf0b7e33d6013988523a04
Wender Rocha <Wender_Dev@hotmail.com>
2021-10-27 16:24:57 -0300

    Project created

---
+++ src/test/java/com/wender/dev/orderServiceapi/OrderServiceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wender.dev.orderServiceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderServiceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
